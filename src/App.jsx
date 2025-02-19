f6c53c79fdd693f31d81d934e7e2fafb4c192291
rahilsk203 <rahilsk203@gmail.com>
2025-02-19 14:53:13 +0300

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-r from-red-50 via-orange-50 to-yellow-50">
      <Navbar />
+     <br />
+     <main className="w-full flex-1" >
+       <div className="w-full" >
+         <Banner />
+       </div>
+     </main>
     </div>
   );
 };
