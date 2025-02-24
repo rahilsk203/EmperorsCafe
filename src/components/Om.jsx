dca0e031e3d101d7611910b39e2ba02d92ea97a2
rahilsk203 <rahilsk203@gmail.com>
2025-02-24 06:51:47 +0300

    react

--- src/components/Om.jsx
+++ src/components/Om.jsx
@@ -35,11 +35,25 @@ const menuItems = [
 ];
 
 export const Om = () => {
+  
+  const[selectedItem, setSelectedItem]= useState(menuItems[0]);
+  
   return (
     <div className='min-h-screen bg-gradient-to-br from-orange-50 to-yellow-50 py-12 px-4 sm:px-6 md:px-8 lg:px-8 flex flex-col md:flex-col lg:flex-row items-center gap-8'>
     {/*LEFT SECTION*/}
     <div className="w-full md:w-3/4 lg:w-1/4 bg-white p-4 md:p-6 rounded-3xl shadow-lg border border-amber-300">
-      <h1></h1>
+      <h1 className="text-3xl md:text-4xl font-pacifico text-amber-600 mb-4 md:mb-6 uppercase text-center">Our Menu</h1>
+      <p className='text-base md:text-lg font-comfortaa text-amber-700 italic mb-4 md:mb-6 text-center'>Menu That Always <br /> Marks You Fall in Love</p>
+      <div className="space-y-3 md:space-y-4 ">
+        {menuItems.map((item,index) => (
+        <button key={index} onClick={() => setSelectedItem(item)} className={`flex items-center justify-between w-full p-2 md:p-3 rounded-2xl transition-all duration-300 ${setSelectedItem.name === item.name ? "bg-amber-600 text-white":"bg-white text-gray-800 border-amber-300 " } hover:bg-amber-500 hover:text-white shadow-md`}>
+          <div className="flex items-center space-x-2 md:space-x-3">
+            {/*ICON CONTAINER*/}
+            <div className={`w-10 h-10 md:w-12 md:h-12 flex items-center justify-center rounded-full bg-gradient-to-r ${setSelectedItem === item.name ? "from-yellow-400 to-orange-500 border-4 border-white shadow-lg scale-105" : "from-gray-200 to-gray-300 border-2 border-transparent"}`}></div>
+          </div>
+        </button>
+        ))}
+      </div>
     </div>
     </div>
   )
